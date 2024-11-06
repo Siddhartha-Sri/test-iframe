24aab7d72a4af3409d183ba8ec9f7543b163b72f
Siddhartha <siddhartha.srivastava@quidcash.in>
2024-11-06 19:43:24 +0530

    changed session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,16 +13,89 @@ export default function Home() {
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          Quid IFrame
+          Hi My Name is Siddhartha
         </ol>
         <div>
           <iframe
-            src="https://pr-469.d1zvoni4pjwmzv.amplifyapp.com?session=eyJhbGciOiJSUzI1NiIsImtpZCI6ImI4Y2FjOTViNGE1YWNkZTBiOTY1NzJkZWU4YzhjOTVlZWU0OGNjY2QiLCJ0eXAiOiJKV1QifQ.eyJpZCI6MTgsImlzX3JlcHJlc2VudGF0aXZlIjpmYWxzZSwiaXNzIjoiaHR0cHM6Ly9zZWN1cmV0b2tlbi5nb29nbGUuY29tL3F1aWRjYXNoLWQ1YTg4IiwiYXVkIjoicXVpZGNhc2gtZDVhODgiLCJhdXRoX3RpbWUiOjE3MzA4Nzc3MTksInVzZXJfaWQiOiJjMjBmNjI2ZC01ZmViLTRmODEtYjI1Yy01ZGUwYTExYThuaXIiLCJzdWIiOiJjMjBmNjI2ZC01ZmViLTRmODEtYjI1Yy01ZGUwYTExYThuaXIiLCJpYXQiOjE3MzA5MDA4MDUsImV4cCI6MTczMDkwNDQwNSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6e30sInNpZ25faW5fcHJvdmlkZXIiOiJjdXN0b20ifX0.wB6H8xxvwjOjwaHy7KTCWfG1uu-Ns-EeVUXl6E2CoVUNvNWV1lqai5Qc3TqUrEVm8khioSmPf7HhnQrIhestSPDFGfjs2TsmDJcDNmO2lUOgOsn32WHDM5I2Xcs5mZzejRyP6NiXsEy6jRYLDKDNWeB3Fkp-snV5X6TtY0keimlB6ZFSfrSvABA1pkEX23sNIPiZpLSmQ9y87bFr_LEexgBPw3-2Rdzj0dKlzFlOLdK-30ob7XXXRLrrJBZLw1NAgsDUCGFa_txGO7Vj1rgGrTDVqcNECmKCbCVlz0mSI7eN26sj36KBnIgXL9rlB6c3Z2vPoKMamYDvoOrdVBQ_eQ&retailer_id=18&iframe_instance='true'"
+            src="https://pr-469.d1zvoni4pjwmzv.amplifyapp.com/hi?session=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTczMDkwMjMzNiwiZXhwIjoxNzMwOTA1OTM2LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay01dDhoNkBxdWlkY2FzaC1kNWE4OC5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInN1YiI6ImZpcmViYXNlLWFkbWluc2RrLTV0OGg2QHF1aWRjYXNoLWQ1YTg4LmlhbS5nc2VydmljZWFjY291bnQuY29tIiwidWlkIjoiYzIwZjYyNmQtNWZlYi00ZjgxLWIyNWMtNWRlMGExMWE4bmlyIiwiY2xhaW1zIjp7ImlkIjoxOCwiaXNfcmVwcmVzZW50YXRpdmUiOmZhbHNlfX0.BBd63AaIfi0Hq01YmG4v9uV6HW-wIMvCm-vz2DyHmFl8po0V2ddFRtp6hgy1hvnnerHta7Vib3bhmBoLvKBlIAUsi7ztFSzMFq-CtrsHuaKyyzIHyEZp0OCg6aOO0S2oJk80h4_Zhyh4xU-AgjPHhMgP5mArYHZNM5DW89DE-C_y-OaYyA-wQRngKjn3gOksBea4FEmnqrXIWdyKujBvFTEJOt5dS6onVG379hJkH-sCesgohljSEaXwN1UBAyyoH_O9r822RGxhacUzGoo1-fCTmTGbKOt2UkJDc7QjnSCIpqpEYtxHNxo5oWv-ereLUJEPKIVmj8aOchlskQBV5A&retailer_id=18&iframe_instance='true'"
             className="h-[1000px] w-[550px]"
             sandbox="allow-same-origin allow-scripts"
           />
         </div>
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
+        </div>
       </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
     </div>
   );
 }
